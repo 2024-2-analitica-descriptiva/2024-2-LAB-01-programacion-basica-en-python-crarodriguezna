523ec4242be5ba7965be18504bb2d7652edddcf4
Cristian Alexander Rodriguez Naranjo <crarodriguezna@unal.edu.co>
2024-11-24 17:16:05 -0500

    Laboratorio 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,7 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
+import csv
 
 
 def pregunta_01():
@@ -14,3 +15,14 @@ def pregunta_01():
     214
 
     """
+    suma = 0
+    with open('files/input/data.csv', mode='r', encoding='utf-8') as archivo:
+        documento = csv.reader(archivo)
+        # next(lector)  # Salta la primera fila (encabezado)
+        for fila in documento:  # Itera sobre las filas restantes
+            try:
+                suma += float(fila[0].replace('\t', '')[1])
+            except (ValueError, IndexError):
+                # Si hay un error en la conversión o el índice no existe, lo ignora
+                pass
+    return suma
